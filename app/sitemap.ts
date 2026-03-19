594cd411ca7382677f4ac99e5ecea568d2cfea29
Joe Filfli <joefilfli48@gmail.com>
2026-03-19 22:49:07 +0200

    Fix: make sitemap dynamic to avoid DB query at build time

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from "next";
 import { prisma } from "@/lib/utils/prisma";
 
+export const dynamic = "force-dynamic";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const base = process.env.NEXTAUTH_URL || "http://localhost:3000";
 
